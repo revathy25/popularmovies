36d413a93573f4575e51e880382d06cf99460670
Revathy Gunasekaran <rgunasek@cablevision.com>
2016-09-19 13:33:24 -0400

    Fixed settings menu click on movie detail page.

--- app/src/main/java/com/example/android/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailActivity.java
@@ -61,7 +61,8 @@ public class DetailActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-           // startActivity(new Intent(this, SettingsActivity.class));
+            Intent intent = new Intent(DetailActivity.this, SettingsActivity.class);
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
